084addcdd061a6cc5227d88e6e5acb2d17e1e15d
Zahra Hadj Ali <76438254+ZahraHadjAli@users.noreply.github.com>
2023-11-11 22:33:35 +0100

    Add files via upload

---
+++ armstrg.py
@@ -0,0 +1,31 @@
+
+#fonction d'armstrong
+def armstrg(ch):
+    try:
+        if verif(ch)== False:
+            print("Le nombre est non valide")
+        else:
+            b=0
+            for i in range(len(ch)):
+                b = b + int(ch[i])**3
+                print(b)
+            if b == int(ch):
+                return 1   
+            else:
+                 return 0
+    except ValueError: 
+        print("Un erreur se produit.") 
+
+#fonction verification
+def verif(arg1):
+    try:
+        if arg1.isnumeric() == False :
+            return False
+        else:
+            if int(arg1) > 0 :
+                return True
+            else:
+                return False
+    except ValueError: 
+        print("Un erreur se produit.") 
+        return False
---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+import armstrg
+
+
+class Test(unittest.TestCase):
+    """
+    Tests pour les fonctions de armstrg
+    """
+
+    def test_valid0(self):
+        self.armstrg(armstrg.armstrg("0"), 1)
+
+    def test_valid1(self):
+        self.armstrg(armstrg.armstrg("1"), 1)
+
+    def test_valid153(self):
+        self.armstrg(armstrg.armstrg("153"), 1)
+
+    def test_valid371(self):
+        self.armstrg(armstrg.armstrg("371"), 1)
+
+    def test_valid407(self):
+        self.armstrg(armstrg.armstrg("407"), 1)
+
+    def test_valid555(self):
+        self.armstrg(armstrg.armstrg("555"), 0)
+
+    def test_valid6(self):
+        self.armstrg(armstrg.armstrg("6"), 0)
+
+    def test_valid905(self):
+        self.armstrg(armstrg.armstrg("905"), 0)
+
+
+    if __name__ == "__main__":
+        unittest.main()
