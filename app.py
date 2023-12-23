16662769ecb1e7a4b3bc0dac0ad9c5bca655251f
ZahraHadjALi <zahrahadjali2012@gmail.com>
2023-12-23 11:52:50 +0100

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # return 'Hello, World!'
-    return '<strong>Bienvenu dans notre mini projet Armstrong .. <br> HADJ ALI Zahra <br> MAGHRAOUI Farah  </strong> '
+    return '<strong>Bienvenu dans notre mini projet Armstrong .. <br> HADJ ALI Zahra <br> <br> MAGHRAOUI Farah  </strong> '
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
